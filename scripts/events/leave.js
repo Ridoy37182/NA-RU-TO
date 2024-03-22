57da66463a98bc696fe0baacf41b8d3bef06a3a4
Ridoy37182 <164547455+Ridoy37182@users.noreply.github.com>
2024-03-23 01:36:06 +0600

    Update leave.js

--- scripts/events/leave.js
+++ scripts/events/leave.js
@@ -14,7 +14,7 @@ module.exports.run = async function({ api, event, Users, Threads }) {
 	const data = global.data.threadData.get(parseInt(threadID)) || (await Threads.getData(threadID)).data;
 	const name = global.data.userName.get(event.logMessageData.leftParticipantFbId) || await Users.getNameUser(event.logMessageData.leftParticipantFbId);
 	const type = (event.author == event.logMessageData.leftParticipantFbId) ? "লিভ নেউয়ার জন্য ধন্যবাদ 🤢" : "Kicked by Administrator";
-	const path = join(__dirname, "nayan", "leaveGif");
+	const path = join(__dirname, "naruto", "leaveGif");
 	const gifPath = join(path, `l.gif`);
 	var msg, formPush
 
